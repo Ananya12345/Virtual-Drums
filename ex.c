75a60c5945a038d657b904e88de157fe91a9115a
Ananya12345 <43907986+Ananya12345@users.noreply.github.com>
2020-03-24 16:58:21 +0530

    Add files via upload

---
+++ ex.c
@@ -0,0 +1,54 @@
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <stdio.h>
+#include<SOIL.h>
+
+int main(int argc, char** argv)
+{
+    //create GL context
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("windowname");
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(0, 800, 0, 600, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+
+    //clear and draw quad with texture (could be in display callback)
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    GLuint tex = SOIL_load_OGL_texture
+	(
+		"frog.png",
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+	);
+	 
+	if( 0 == tex )
+
+		printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+
+  
+    glBindTexture(GL_TEXTURE_2D, tex);
+   // glPushMatrix();
+    
+    
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_QUADS);
+    glTexCoord2i(0, 0); glVertex2i(100, 100);
+    glTexCoord2i(0, 1); glVertex2i(100, 500);
+    glTexCoord2i(1, 1); glVertex2i(500, 500);
+    glTexCoord2i(1, 0); glVertex2i(500, 100);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    
+    //glPopMatrix();
+    glFlush(); //if you remove this there is a 
+
+    getchar(); //pause so you can see what just happened
+    //if you remove this, it doesnt work
+    //System("pause"); //I think this works on windows
+
+    return 0;
+}
